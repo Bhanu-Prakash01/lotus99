2b7cba4359324e07ea388fac27a71d10b13e9591
Bhanu-Prakash01 <kingkite44@gmail.com>
2024-07-09 16:57:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className='creds'>
+            <div className="options">
+               <button className='op1 a' style={{"backgroundColor":"#F4D93B" ,"color":"black"}}> SignUp</button>
+               <button className='op1' style={{"backgroundColor":"#185685"}}> Login</button>
+              </div>
+              <div className="creds_div">
+                <br/>
+                  <img class="creds_img" src="logo.jpg" alt='logo'/>
+                  {/* <br/> */}
+                  <br/>
+                  <input placeholder='Your Username' type="text"/>
+                  <br/>
+
+                  <input placeholder='Your Password' type="text"/>
+                  <br/>
+
+                  <button className='op1_b' style={{"backgroundColor":"#185685"}} onClick={()=>alert("Unable to login ")}> Login</button>
+                  <br/>
+                  <button className='op1_b' style={{"backgroundColor":"#185685"}} onClick={()=>alert("Unable to login ")}> Login as demo user</button>
+              </div>
+       </div>
     </div>
   );
 }
